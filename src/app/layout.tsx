c6b4938a79b908700f6527cfe4f6b0e827d42724
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-06-08 23:04:23 +0000

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import { Baloo_2 as Baloo, Roboto_Flex as Roboto } from "next/font/google";
+
+const baloo = Baloo({ subsets: ["latin"], variable: "--baloo-font" });
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--roboto-font",
+});
+
+export const metadata = {
+  title: "Coffee Delivery",
+  description: "Desafio do Projeto 2 do bootcamp Ignite Rocketseat.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${roboto.variable} ${baloo.variable} min-h-screen font-sans`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="font-title ">Coffee Delivery</h1>
+    </div>
+  );
+}
